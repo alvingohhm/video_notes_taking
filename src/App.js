0d4f74da815ee5f51001f787f52ffbbebae7067b
alvingohhm <alvgoh@yahoo.com>
2021-12-15 01:19:23 +0800

    setup routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
@@ -6,8 +7,9 @@ function App() {
   return (
     <>
       <Header />
-
-      <Main />
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
     </>
   );
 }
